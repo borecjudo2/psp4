db4c4a1be4fbe7aa97a0a286d72fb9d2d6f47dbf
Vladislav Karpeka <Vlad.Karpeka@vertexinc.com>
2022-01-09 14:42:13 +0300

    init commit

---
+++ src/main/java/by/bsuir/psp4/Psp4Application.java
@@ -0,0 +1,13 @@
+package by.bsuir.psp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Psp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Psp4Application.class, args);
+	}
+
+}
